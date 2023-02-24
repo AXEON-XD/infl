48ce2add53dc146acb0ff702594cf87934f06779
AXEON-XD <99555142+AXEON-XD@users.noreply.github.com>
2023-02-24 12:16:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,8 +25,8 @@ UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/AnonymousX1025/AnonX
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/KRISTY_MF")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/KRISTY_AF")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/TEAM_XTRON")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/TEAMXTRON")
 
 SUPPORT_HEHE = SUPPORT_GROUP.split("me/")[1]
 
